d70273626679a339b287a9d73ff83539b2812189
ashutosh <ashutosh.choudhary_cs21@gla.ac.in>
2025-01-17 12:20:50 +0530

    first commit

--- Multiplication.java
+++ Multiplication.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Multiplication {
+public class multiplication {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
